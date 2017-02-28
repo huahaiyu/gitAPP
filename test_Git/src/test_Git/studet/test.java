dae42faaaa905700e1336f1843285356e25458e6
huahaiyu <736801828@qq.com>
2017-02-28 18:16:37 +0800

    第二次提交代码

---
+++ test_Git/src/test_Git/studet/test.java
@@ -0,0 +1,37 @@
+/**  
+* @Title: test.java
+* @Package test_Git.studet
+* @Description: TODO(用一句话描述该文件做什么)
+* @author A18ccms A18ccms_gmail_com  
+* @date 2017年2月28日 下午5:55:43
+* @version V1.0  
+*/
+package test_Git.studet;
+
+/**
+* @ClassName: test
+* @Description: TODO(这里用一句话描述这个类的作用)
+* @author A18ccms a18ccms_gmail_com
+* @date 2017年2月28日 下午5:55:43
+*
+*/
+public class test {
+
+	/**
+	* @Title: main
+	* @Description: TODO(这里用一句话描述这个方法的作用)
+	* @param @param args    设定文件
+	* @return void    返回类型
+	* @throws
+	*/
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+		int s = a + b;
+		int x = 1;
+		
+		x = x * 2 + 1;
+		System.out.println("hahha"+x);
+	}
+
+}
